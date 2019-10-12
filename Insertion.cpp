bc8a13d41b33e777dec053854f14006772d2883b
Arpit Sharma <1arpit2000sharma@gmail.com>
2019-10-12 19:39:50 +0530

    insertion

---
+++ Insertion.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int value;
+    struct node *next;
+};
+
+node *createNode(int value)
+{
+    node *n1 = new node;
+    n1->value = value;
+    n1->next = NULL;
+    return n1;
+};
+
+class LinkedList
+{
+    node *head;
+    public:
+    LinkedList()
+    {
+        head = NULL;
+    }
+    void insertStart(int value)
+    {
+        node *n1 = createNode(value);
+        if(head == NULL)
+        {
+            head = n1;
+            return;
+        }
+        n1->next = head;
+        head = n1;
+    }
+     void displayAll()
+    {
+        if (head == NULL)
+            cout << "UnderFlow";
+        else
+        {
+            node *ptr = head;
+            while (ptr != NULL)
+            {
+                cout << ptr->value << "->";
+                ptr = ptr->next;
+            }
+            cout << endl;
+        }
+}
+};
+int main()
+{
+    //comment
+    int i,k;
+    LinkedList L;
+    for(i=0;i<5;i++)
+    {
+        cin>>k;
+        L.insertStart(k);
+    }
+    //L.insertStart(5);
+    L.displayAll();
+    
+    return 0;
+}
